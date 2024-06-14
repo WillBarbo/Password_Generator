e6c72eeca45947d8789bc087aa78a5bea39b2eb6
Willian Barbosa <willianbsilva96@gmail.com>
2024-06-14 16:32:57 -0300

    vercel debug

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import PasswordGenerator from './modules/PasswordGenerator'
 
+import "./assets/css/style.css"
+
 let arrayNum = [1, 2, 3, 4]
 
 let numUser2 = (document.querySelector('.char').value = 11)
